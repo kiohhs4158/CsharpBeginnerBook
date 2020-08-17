b1101d6ca05879a00b792a0367ecba134610b5e8
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-17 17:37:55 +0900

    Finish sample 209

---
+++ sumple209/sumple209/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumple209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1から3の数字を入力してください");
+            int num = int.Parse(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("three");
+            }
+            else
+            {
+                Console.WriteLine("不適切な値が入力されました");
+            }
+        }
+    }
+}
