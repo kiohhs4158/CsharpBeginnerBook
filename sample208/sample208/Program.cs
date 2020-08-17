432d48ede7415aff22b6130569056e2656959225
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-17 17:29:08 +0900

    Finish sumple208

---
+++ sample208/sample208/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力してください：");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = " + a);
+            if (a > 0)
+            {
+                Console.WriteLine("入力した値は自然数です");
+            }
+            else
+            {
+                Console.WriteLine("入力した値は正の整数ではありません");
+            }
+        }
+    }
+}
