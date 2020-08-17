4df6e72966ae0633db5ba9378baa47a8a4cd6fc2
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-17 16:14:22 +0900

    Finish sample204

---
+++ sample204/sample204/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, d;
+            int c;
+
+            a = 1.23;
+            b = (double)1.23;// キャストありで代入
+            c = (int)1.23;//キャストなしで代入
+            d = 10;//キャストなしで代入
+
+            Console.WriteLine("a = {0}, b = {1}, c = {2}, d = {3}", a, b, c, d);
+        }
+    }
+}
