37ab76b88b9a4bd34e356defffa9e2318b598a58
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-17 15:30:49 +0900

    Create repository for C#

---
+++ sample201/sample201/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 加算
+            Console.WriteLine("{0} + {1} = {2}", 5, 2, 5 + 2);
+            // 減算
+            Console.WriteLine("{0} - {1} = {2}", 5, 2, 5 - 2);
+            // 乗算
+            Console.WriteLine("{0} * {1} = {2}", 5, 2, 5 * 2);
+            // 除算
+            Console.WriteLine("{0} / {1} = {2} 余り {3}", 5, 2, 5 / 2, 5 % 2);
+        }
+    }
+}
---
+++ sample202/sample202/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;// 変数の宣言
+            int b = 3;// 宣言と初期化を同時に行う
+            int add, sub;// 複数の変数を同時に宣言
+            double avg;// int以外の変数を宣言
+            a = 6;
+            add = a + b;
+            sub = a - b;
+            avg = (a + b) / 2.0;
+            Console.WriteLine("{0} + {1} = {2}", a, b, add);
+            Console.WriteLine("{0} - {1} = {2}", a, b, sub);
+            Console.WriteLine("{0}と{1}の平均は{2}", a, b, avg);
+
+        }
+    }
+}
