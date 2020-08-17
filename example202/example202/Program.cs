2e9623a8ee5e38374f7a0da770b4955b999c7ace
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-17 17:10:37 +0900

    Finish example 2-1

---
+++ example202/example202/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom";
+            Console.WriteLine("My name is {0}", name);
+        }
+    }
+}
---
+++ example203/example203/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, avg;
+            Console.Write("num1 = ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("num2 = ");
+            num2 = double.Parse(Console.ReadLine());
+            avg = (num1 + num2) / 2.0;
+            Console.WriteLine("num1とnum2の平均値は{0}です", avg);
+        }
+    }
+}
