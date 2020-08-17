6cb7193e4236dc228942c416adcf1562a366421e
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-17 17:16:22 +0900

    Finish sample207

---
+++ sample207/sample207/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数値を入力してください:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("a = " + a);
+            if (a > 0)
+                Console.WriteLine("入力された値は正の数です");
+        }
+    }
+}
