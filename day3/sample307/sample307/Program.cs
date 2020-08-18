0eae0261b5abf1772916f4a688f470232b605b5a
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-18 17:48:26 +0900

    Finish array sample

---
+++ day3/sample307/sample307/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 2, 3, 4 };
+            foreach (int i in n)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
