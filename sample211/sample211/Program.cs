e35b14508e582ff5bcd9b78ad70f9513846d2e3f
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-18 09:48:39 +0900

    finish sample211

---
+++ sample211/sample211/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1から3の数字を入力：");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+        }
+    }
+}
