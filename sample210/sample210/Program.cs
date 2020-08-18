a923cb55e9c7fe72fda1968d0c6b7635347faf30
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-18 09:32:00 +0900

    finish sample210

---
+++ sample210/sample210/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // さいころの目
+            Console.Write("サイコロの目（1-6）を入力してください：");
+            int dice = int.Parse(Console.ReadLine());
+            if (1 <= dice && dice <= 6)
+            {
+                if (dice % 2 == 0)
+                {
+                    Console.WriteLine("丁です");
+                }
+                else
+                {
+                    Console.WriteLine("半です");
+                }
+            }
+            else
+            {
+                Console.WriteLine("範囲外の数値です");
+            }
+        }
+    }
+}
