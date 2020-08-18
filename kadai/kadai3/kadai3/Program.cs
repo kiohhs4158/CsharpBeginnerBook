b42252f9dc5fb050eaa20e651c14ed9ead40ac13
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-18 15:47:24 +0900

    課題（九九表）完了

---
+++ kadai/kadai3/kadai3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    int ans = i * j;
+                    Console.Write(ans + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
