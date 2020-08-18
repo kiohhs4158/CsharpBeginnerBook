3c5ac29581842b4cf64c87a7935e90b670e051c7
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-18 10:08:45 +0900

    finsih sample212

---
+++ sample212/sample212/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("サイコロの目を入力してください：");
+            int dice = int.Parse(Console.ReadLine());
+            switch (dice)
+            {
+                case 1:
+                case 3:
+                case 5:
+                    Console.WriteLine("奇数です");
+                    break;
+                case 2:
+                case 4:
+                case 6:
+                    Console.WriteLine("偶数です");
+                    break;
+                default:
+                    Console.WriteLine("範囲外の数値です");
+                    break;
+            }
+        }
+    }
+}
