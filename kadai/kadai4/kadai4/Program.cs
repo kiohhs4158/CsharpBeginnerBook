aa35c5a14b8bf74df6647a9f23d15e3dddbd87a7
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-19 14:08:54 +0900

    課題4（階段）完了

---
+++ kadai/kadai4/kadai4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 階段
+            string[,] stairs = new string[5, 5];
+            for (int i = 0; i < stairs.GetLength(0); i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    stairs[i, j] = "■";
+                    Console.Write(stairs[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
