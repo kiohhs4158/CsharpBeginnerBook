0dfd3ce3deda65160b4cb038ff5b52b622d5206d
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-19 13:48:45 +0900

    課題2（バブルソート）完了

--- kadai/kadai2/kadai2/Program.cs
+++ kadai/kadai2/kadai2/Program.cs
@@ -11,6 +11,33 @@ namespace kadai2
         static void Main(string[] args)
         {
             // バブルソートアルゴリズム
+            // 配列を定義
+            int[] array = { 8, 4, 3, 7, 6, 5, 2, 1 };
+            Console.WriteLine("元の配列");
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int x = i + 1;
+                for (int j = 0; j < array.Length - x; j++)
+                {
+                    if(array[j] < array[j + 1])
+                    {
+                        int replace = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = replace;
+                    }
+                }
+            }
+            Console.WriteLine("バブルソート");
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
         }
     }
 }
