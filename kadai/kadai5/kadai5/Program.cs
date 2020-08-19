7eca03135cf7b187b0e58423e6b6b5a00592b5ed
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-19 14:40:07 +0900

    課題5（ピラミッド）完了

---
+++ kadai/kadai5/kadai5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] pyramid = new string[4, 7];
+            const string PARTS = "■";
+            for (int i = 0; i < pyramid.GetLength(0); i++)
+            {
+                for (int j = 0; j < pyramid.GetLength(1); j++)
+                {
+                    pyramid[i, j] = "　";
+                }
+            }
+            for (int i = 0; i < pyramid.GetLength(0); i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    pyramid[i, 3] = PARTS;
+                    pyramid[i, 3 + j] = PARTS;
+                    pyramid[i, 3 - j] = PARTS;
+                }
+            }
+            for (int i = 0; i < pyramid.GetLength(0); i++)
+            {
+                for (int j = 0; j < pyramid.GetLength(1); j++)
+                {
+                    Console.Write(pyramid[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
