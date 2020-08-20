054aeb41924f06c551c58cab840bea9bcf3f60ae
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-20 11:05:40 +0900

    Finish constructor sample

---
+++ day4/Sample406/Sample406/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample406
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1, p2;
+            p1 = new Person();
+            p2 = new Person("Hirofumi Kunimitsu", 23);
+            p1.Name = "Kohei Ibayashi";
+            p1.Age = 27;
+            p1.ShowAgeAndName();
+            p2.ShowAgeAndName();
+        }
+    }
+}
---
+++ day4/Sample407/Sample407/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample407
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] a = new String[10000];
+            for (int i = 0; i < 10000; i++)
+            {
+                a[i] = new String('K', 10000);
+            }
+            Console.WriteLine("メモリ使用量（GC発動前）：" + GC.GetTotalMemory(false));
+            // aの参照を開放
+            a = null;
+            Console.WriteLine("メモリ使用量（参照解放後）：" + GC.GetTotalMemory(false));
+            GC.Collect();
+            Console.WriteLine("メモリ使用量（GC発動後）：" + GC.GetTotalMemory(false));
+        }
+    }
+}
