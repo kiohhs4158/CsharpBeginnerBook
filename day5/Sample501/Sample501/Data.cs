c78bc4dea407b789a760c96708651a02b31c8523
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-20 15:30:51 +0900

    Finish day5 static sample

---
+++ day5/Sample501/Sample501/Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample501
+{
+    class Data
+    {
+        private static int num = 0;
+        private int id;
+        public Data(int id)
+        {
+            this.id = id;
+            num++;
+            Console.WriteLine("id{0}、データの数{1}", this.id, num);
+        }
+        public static void ShowNumber()
+        {
+            Console.WriteLine("Dataのオブジェクトの数{0}", num);
+        }
+    }
+}
---
+++ day5/Sample502/Sample502/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample502
+{
+    class Program
+    {
+        private static int snum = 100;
+        public int inum = 200;
+        public static void Foo()
+        {
+            Console.WriteLine("fooメソッド(staticメソッド)");
+        }
+        public void Bar()
+        {
+            Console.WriteLine("barメソッド(インスタンスメソッド)");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("pのインスタンスフィールド：{0}", p.inum);
+            Console.WriteLine("pのクラスフィールド：{0}", snum);
+            Foo();
+            p.Bar();
+        }
+    }
+}
