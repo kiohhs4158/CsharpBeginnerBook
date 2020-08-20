4d6dfdf57150d034802e77a2ba5b31381ae41fbd
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-20 15:58:58 +0900

    Finish static example

---
+++ day5/Example502/Example502/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("角度を入力：");
+            double angle = double.Parse(Console.ReadLine());
+            double rad = Math.PI * angle / 180.0;
+            Console.WriteLine("sin{0}° = {1}", angle, Math.Sin(rad));
+            Console.WriteLine("cos{0}° = {1}", angle, Math.Cos(rad));
+            Console.WriteLine("tan{0}° = {1}", angle, Math.Tan(rad));
+        }
+    }
+}
