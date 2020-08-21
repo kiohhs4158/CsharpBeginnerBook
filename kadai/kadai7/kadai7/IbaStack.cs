6b6dbfdb04d6aa38a4ddc2ee146548588ddd8dc3
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-21 13:23:04 +0900

    課題7（スタック構造クラス）完了

---
+++ kadai/kadai7/kadai7/IbaStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadai7
+{
+    class IbaStack
+    {   
+        private string[] data;
+        public int num = 0;
+
+        public IbaStack(int size)
+        {
+            data = new string[size];
+        }
+
+        public void Push(string fruits)
+        {
+            data[num] = fruits;
+            num++;
+        }
+
+        public string Pop()
+        {
+            num--;
+            return data[num];
+        }
+    }
+}
---
+++ kadai/kadai7/kadai7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadai7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IbaStack ibastack = new IbaStack(3);
+            ibastack.Push("apple");
+            ibastack.Push("banana");
+            ibastack.Push("orange");
+
+            Console.WriteLine(ibastack.Pop());
+            Console.WriteLine(ibastack.Pop());
+            Console.WriteLine(ibastack.Pop());
+        }
+    }
+}
