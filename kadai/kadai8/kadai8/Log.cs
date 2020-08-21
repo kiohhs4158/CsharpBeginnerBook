e75fb01af2f78224d302ebba2edf315e85dec824
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-21 17:18:21 +0900

    課題8（ログ出力）完了

---
+++ kadai/kadai8/kadai8/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadai8
+{
+    class Log
+    {
+        private static StreamWriter writer;
+        private static DateTime dt = DateTime.Now;
+        public static void Write(string text)
+        {
+            string path = "D:\\CsharpBeginnerBook\\kadai\\kadai8\\kadai8\\kadai8.txt";
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");
+            writer = new StreamWriter(@path, true, enc);
+            writer.WriteLine($"[{dt}:{dt.Millisecond}] {text}");
+            writer.Close();
+        }
+
+    }
+}
---
+++ kadai/kadai8/kadai8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadai8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("テキストを入力してください：");
+            string text = Console.ReadLine();
+            Log.Write(text);
+            Console.WriteLine("終了します");
+        }
+    }
+}
