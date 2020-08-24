92c1bd8132d0513bbabae3f0c93b67c5889c8989
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-08-24 15:28:23 +0900

    課題9（クラスライブラリの活用）完了

---
+++ kadai/kadai9/kadai9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace kadai9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            Console.Write("文字列を入力してください：");
+            string text = Console.ReadLine();
+
+            log.Debug(text);
+            log.Info(text);
+            log.Warn(text);
+            log.Error(text);
+            log.Fatal(text);
+        }
+    }
+}
